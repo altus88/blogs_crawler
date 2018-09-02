f82d0fa2a072424f051b93a7d8aabe82b14a2485
Gennady Shabanov <gennady.shabanov.rus@gmail.com>
2018-09-02 14:18:25 +0200

    be able to accept requests from frontend

--- src/main/java/com/example/easynotes/controller/ItemController.java
+++ src/main/java/com/example/easynotes/controller/ItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,7 @@ public class ItemController
 
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/items/text/{search}")
 	public List<Item> getItemByText(@PathVariable(value = "search") String search) {
 
